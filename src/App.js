039f9815cc018968157cbcb41dbde73f8fc5d4ae
Ishan-Pradhan <pradhan.ishan70@gmail.com>
2023-12-02 14:01:37 +0545

    added edit function

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ export default function App() {
     return JSON.parse(localValue);
   });
 
+  const [editingId, setEditingId] = useState(null);
+  const [editedTodo, setEditedTodo] = useState("");
+
   useEffect(() => {
     localStorage.setItem("LISTS", JSON.stringify(lists));
   }, [lists]);
@@ -29,6 +32,23 @@ export default function App() {
     setLists((lists) => lists.filter((list) => list.id !== id));
   }
 
+  function handleUpdate() {
+    setLists((previousList) =>
+      previousList.map((list) =>
+        list.id === editingId ? { ...list, todo: editedTodo } : list
+      )
+    );
+    setEditingId(null);
+    setEditedTodo("");
+  }
+
+  function handleEdit(id) {
+    const taskToEdit = lists.find((list) => list.id === id);
+    console.log(taskToEdit.todo);
+    setEditingId(id);
+    setEditedTodo(taskToEdit.todo);
+  }
+
   return (
     <div className="container border-0 max-w-sm p-1  md:container md:max-w-xl md:border md:p-10 m-auto mt-20 ">
       <div className="max-w-sm  flex justify-center flex-col items-center m-auto md:max-w-[500px]">
@@ -38,6 +58,12 @@ export default function App() {
           lists={lists}
           onToggleList={handleToggleList}
           onHandleDelete={handleDeleteList}
+          editingId={editingId}
+          setEditingId={setEditingId}
+          editedTodo={editedTodo}
+          setEditedTodo={setEditedTodo}
+          onhandleEdit={handleEdit}
+          onHandleUpdate={handleUpdate}
         />
       </div>
     </div>
@@ -50,7 +76,6 @@ function Logo() {
 
 function Form({ onAddLists }) {
   const [todo, setTodo] = useState("");
-
   function handleSubmit(e) {
     e.preventDefault();
 
@@ -79,7 +104,17 @@ function Form({ onAddLists }) {
   );
 }
 
-function Lists({ lists, onToggleList, onHandleDelete }) {
+function Lists({
+  lists,
+  onToggleList,
+  onHandleDelete,
+  editingId,
+  setEditingId,
+  editedTodo,
+  setEditedTodo,
+  onHandleUpdate,
+  onhandleEdit,
+}) {
   return (
     <ul className="w-full">
       {lists.map((list) => (
@@ -88,13 +123,29 @@ function Lists({ lists, onToggleList, onHandleDelete }) {
           onToggleList={onToggleList}
           onHandleDelete={onHandleDelete}
           key={list.id}
+          editingId={editingId}
+          setEditingId={setEditingId}
+          editedTodo={editedTodo}
+          setEditedTodo={setEditedTodo}
+          onHandleUpdate={onHandleUpdate}
+          onhandleEdit={onhandleEdit}
         />
       ))}
     </ul>
   );
 }
 
-function TodoWork({ list, onToggleList, onHandleDelete }) {
+function TodoWork({
+  list,
+  onToggleList,
+  onHandleDelete,
+  onHandleUpdate,
+  onhandleEdit,
+  editingId,
+  setEditingId,
+  editedTodo,
+  setEditedTodo,
+}) {
   return (
     <div>
       <li className="flex  gap-4 justify-between items-center border-b border-b-yellow-500 p-3 w-full ">
@@ -106,25 +157,47 @@ function TodoWork({ list, onToggleList, onHandleDelete }) {
             }}
             checked={list.completed}
           />
-          <span
-            className="text-xl"
-            style={
-              list.completed
-                ? {
-                    textDecoration: "line-through",
 
-                    color: "#777",
-                  }
-                : {}
-            }
-          >
-            {list.todo}
-          </span>
+          {editingId === list.id ? (
+            <input
+              className="outline outline-1 "
+              type="text"
+              value={editedTodo}
+              onChange={(e) => setEditedTodo(e.target.value)}
+            />
+          ) : (
+            <span
+              className="text-xl"
+              style={
+                list.completed
+                  ? {
+                      textDecoration: "line-through",
+
+                      color: "#777",
+                    }
+                  : {}
+              }
+            >
+              {list.todo}
+            </span>
+          )}
         </div>
         <div className="flex justify-center items-center gap-2 ">
-          <button className="text-2xl text-blue-500 hover:text-blue-700">
-            &#x2710;
-          </button>
+          {editingId === list.id ? (
+            <button
+              className="text-2xl text-green-500 hover:text-green-700"
+              onClick={onHandleUpdate}
+            >
+              save
+            </button>
+          ) : (
+            <button
+              className="text-2xl text-blue-500 hover:text-blue-700"
+              onClick={() => onhandleEdit(list.id)}
+            >
+              &#x2710;
+            </button>
+          )}
           <button
             className="text-4xl text-red-500 hover:text-red-700"
             onClick={() => onHandleDelete(list.id)}
